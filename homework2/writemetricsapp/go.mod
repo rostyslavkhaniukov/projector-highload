176a0aafa9fe4351a6a36bc6659931cf4c7056aa
Rost Khaniukov <khaniukov.rostyslav@pdffiller.team>
2022-09-01 15:36:07 +0300

    homework 2

--- homework2/writemetricsapp/go.mod
+++ homework2/writemetricsapp/go.mod
@@ -1,5 +1,23 @@
 module github.com/rostyslavkhaniukov/writemetricsapp
 
-go 1.17
+go 1.18
 
-require github.com/cactus/go-statsd-client/v5 v5.0.0
+require (
+	github.com/cactus/go-statsd-client/v5 v5.0.0
+	github.com/elastic/go-elasticsearch/v8 v8.4.0
+)
+
+require github.com/elastic/elastic-transport-go/v8 v8.1.0 // indirect
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
+	go.mongodb.org/mongo-driver v1.3.1
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- homework2/writemetricsapp/main.go
+++ homework2/writemetricsapp/main.go
@@ -1,17 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/cactus/go-statsd-client/v5/statsd"
+	"github.com/elastic/go-elasticsearch/v8"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"math/rand"
+	"time"
 )
-import "github.com/cactus/go-statsd-client/v5/statsd"
+
+const min = 0
+
+const max = 100
+
+var ctx = context.TODO()
 
 func main() {
-	fmt.Println("Hello")
+	somethingWithMongoDB()
+	somethingWithES()
+	somethingWithOwnMetrics()
+}
 
+func somethingWithMongoDB() {
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = client.Database("test").Collection("test")
+}
+
+func somethingWithES() {
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	defer res.Body.Close()
+}
+
+func somethingWithOwnMetrics() {
 	config := &statsd.ClientConfig{
-		Address: "127.0.0.1:8125",
-		Prefix:  "test-client",
+		Address: "telegraf:8125",
+		Prefix:  "projector",
 	}
 	client, err := statsd.NewClientWithConfig(config)
 	if err != nil {
@@ -19,5 +63,10 @@ func main() {
 	}
 
 	defer client.Close()
-	client.Inc("stat1", 42, 1.0)
+	for {
+		rand.Seed(time.Now().UnixNano())
+		time.Sleep(3 * time.Second)
+		client.Inc("users", rand.Int63n(max-min)+min, 1.0)
+		fmt.Println("Metric has been written")
+	}
 }
